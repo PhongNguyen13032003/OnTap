a2321e47692e5a9a48c435805233f79a56b3c7ce
Phong <nnphong130303@gmail.com>
2023-11-01 16:08:47 +0700

    QuanLiSV

---
+++ QuanLiSV/QuanLiSV/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLiSV
+{
+    public partial class Form1 : Form
+    {
+        KetNoi kn = new KetNoi();
+        SqlConnection connsql;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            connsql = kn.connect;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "Khong duoc de trong");
+            else
+                this.errorProvider1.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "Khong duoc de trong");
+            else
+                this.errorProvider1.Clear();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "Khong duoc de trong");
+            else
+                this.errorProvider1.Clear();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "Khong duoc de trong");
+            else
+                this.errorProvider1.Clear();
+        }
+    }
+}
---
+++ QuanLiSV/QuanLiSV/KetNoi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace QuanLiSV
+{
+    class KetNoi
+    {
+        public SqlConnection connect;
+        public KetNoi()
+        {
+            connect = new SqlConnection("Data Soure=<A209PC33>; Intial Catalog= <Phong>; Ingerated Security = True");
+        }
+        public KetNoi(string strcn)
+        {
+            connect = new SqlConnection(strcn);
+        }
+    }
+}
