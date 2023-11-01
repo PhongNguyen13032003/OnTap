a31114d71788f43fe4a2f951491f3d28ec34736e
Phong <nnphong130303@gmail.com>
2023-11-01 15:10:59 +0700

    first commit

---
+++ QuanLiMH/QuanLiMH/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace QuanLiMH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Them = new System.Windows.Forms.Button();
+            this.Sua = new System.Windows.Forms.Button();
+            this.Xoa = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.MaMH = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Them
+            // 
+            this.Them.Location = new System.Drawing.Point(12, 165);
+            this.Them.Name = "Them";
+            this.Them.Size = new System.Drawing.Size(75, 23);
+            this.Them.TabIndex = 0;
+            this.Them.Text = "Thêm";
+            this.Them.UseVisualStyleBackColor = true;
+            this.Them.Click += new System.EventHandler(this.Them_Click);
+            // 
+            // Sua
+            // 
+            this.Sua.Location = new System.Drawing.Point(103, 165);
+            this.Sua.Name = "Sua";
+            this.Sua.Size = new System.Drawing.Size(75, 23);
+            this.Sua.TabIndex = 0;
+            this.Sua.Text = "Sửa";
+            this.Sua.UseVisualStyleBackColor = true;
+            this.Sua.Click += new System.EventHandler(this.Sua_Click);
+            // 
+            // Xoa
+            // 
+            this.Xoa.Location = new System.Drawing.Point(197, 165);
+            this.Xoa.Name = "Xoa";
+            this.Xoa.Size = new System.Drawing.Size(75, 23);
+            this.Xoa.TabIndex = 0;
+            this.Xoa.Text = "Xóa";
+            this.Xoa.UseVisualStyleBackColor = true;
+            this.Xoa.Click += new System.EventHandler(this.Xoa_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // MaMH
+            // 
+            this.MaMH.AutoSize = true;
+            this.MaMH.Location = new System.Drawing.Point(24, 51);
+            this.MaMH.Name = "MaMH";
+            this.MaMH.Size = new System.Drawing.Size(42, 13);
+            this.MaMH.TabIndex = 2;
+            this.MaMH.Text = "Mã MH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên MH";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MaMH);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Xoa);
+            this.Controls.Add(this.Sua);
+            this.Controls.Add(this.Them);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Them;
+        private System.Windows.Forms.Button Sua;
+        private System.Windows.Forms.Button Xoa;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label MaMH;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ QuanLiMH/QuanLiMH/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLiMH
+{
+    public partial class Form1 : Form
+    {
+        public class KetNoi
+        {
+            public SqlConnection connect;
+            public KetNoi()
+            {
+                connect = new SqlConnection("Data Source = <A209PC33>; Initial Catalog = <Phong>; Integrated Security = True");
+            }
+            public KetNoi(string strcn)
+            {
+                connect = new SqlConnection(strcn);
+            }
+        }
+        KetNoi kn = new KetNoi();
+        SqlConnection connsql;
+        public Form1()
+        {
+            InitializeComponent();
+            connsql = kn.connect;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Them_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connsql.State == ConnectionState.Closed)
+                {
+                    connsql.Open();
+                }
+                string insertString;
+                insertString = "Inser into Phong values(' " + textBox1.Text + " ', N'" + textBox2.Text + "')";
+                SqlCommand cmd = new SqlCommand(insertString, connsql);
+                cmd.ExecuteNonQuery();
+                if (connsql.State == ConnectionState.Open)
+                {
+                    connsql.Close();
+                }
+                MessageBox.Show("Thanh Cong");
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("That Bai");
+            }
+        }
+
+        private void Sua_Click(object sender, EventArgs e)
+        {
+            string updateString = "update MaMH set TenMH= ' " + textBox1.Text + "where MaMH'" + textBox2.Text + "'";
+        }
+
+        private void Xoa_Click(object sender, EventArgs e)
+        {
+            string deleteString = "delete TenMH where MaMH='" + textBox1.Text + "'";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr=(Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "Khong duoc de trong");
+            else
+                this.errorProvider1.Clear();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Control ctr=(Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+                this.errorProvider1.SetError(ctr, "Khong duoc de trong");
+            else
+                this.errorProvider1.Clear();
+        }
+    }
+}
